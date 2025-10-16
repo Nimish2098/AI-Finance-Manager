eb1d5ebf05819e592712420e9af7f4894ec9fa51
Nimish2098 <raonimish23@gmail.com>
2025-10-16 22:26:52 +0530

    New Files added

--- src/main/java/com/Nimish/AIFinanceManager/controller/AuthController.java
+++ src/main/java/com/Nimish/AIFinanceManager/controller/AuthController.java
@@ -1,9 +1,25 @@
 package com.Nimish.AIFinanceManager.controller;
 
+import com.Nimish.AIFinanceManager.dto.RegisterRequest;
+import com.Nimish.AIFinanceManager.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<String> register(@PathVariable  RegisterRequest request){
+
+        
+    }
 
 }
