0bf814528a47bf4a933947f0dbacb25e590fdcdc
Nimish2098 <raonimish23@gmail.com>
2026-01-13 23:47:39 +0530

    Final Pus

---
+++ src/main/java/com/project/financeApp/security/OpenAPIConfig.java
@@ -0,0 +1,35 @@
+package com.project.financeApp.security;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI financeTrackerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Personal Finance Tracker API")
+                        .description("APIs for transactions, accounts, budgets, insights")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
+}
+
+
+
